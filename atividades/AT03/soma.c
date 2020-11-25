9ddc67dca9e2ce419ef17a29169bb5ea1b09f127
luis-garridodev <proganalistaluis@gmail.com>
2020-11-24 21:16:12 -0300

    atualizado

--- atividades/AT03/soma.c
+++ atividades/AT03/soma.c
@@ -2,14 +2,14 @@
 
 
 int contadenumeros(int a,int b){
-  int rsoma1;
-  int rsoma2;
-  int resultado;
-  rsoma1=a;
-  rsoma2=b;
-  resultado=rsoma1+rsoma2;
-  return resultado;
-
+	int rsoma1;
+	int rsoma2;
+	int resultado;
+	rsoma1=a;
+	rsoma2=b;
+	resultado=rsoma1+rsoma2;
+	return resultado;
+	
 }
 
 

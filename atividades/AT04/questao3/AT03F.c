b00b8b139e2445428b57ac0c81f59c9d4dc7432f
luis-garridodev <proganalistaluis@gmail.com>
2020-12-01 21:04:03 -0300

    atividades e pasta atualizadas de 1 a 3

---
+++ atividades/AT04/questao3/AT03F.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int fatorial(int nf){
+	int a=nf;
+	int conta=1;
+	
+	
+	if(nf==-1){
+	    
+		return -1;
+	}	
+	for(int i=a;i>1;i--){
+		
+		conta=conta*i;
+	    
+	}
+	
+	
+	
+	
+	
+	
+	
+	return conta;
+}
+
+
+
+
+
+
+
+int main(){
+	int resultado;
+	
+	int numero;
+	printf("digite o fatorial:\n");
+	scanf("%d",&numero);
+	resultado=fatorial(numero);
+	
+	if(resultado == -1){
+	printf("Não é possível calcular o fatorial do número informado\n");}
+	else{
+		printf("resposta da questao:%d\n",resultado);
+	}
+	
+	system("pause");
+	return 1;
+	
+	
+}
+
+
+
+
+

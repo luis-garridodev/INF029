5b2538ec88eb3997176dff31fa616e589f806f3c
luis-garridodev <proganalistaluis@gmail.com>
2020-12-07 17:10:48 -0300

    atualizada

--- atividades/AT04/questao6/AT06F.c
+++ atividades/AT04/questao6/AT06F.c
@@ -1,33 +1,39 @@
 	#include<stdio.h>
 	#include<stdlib.h>
 
-	char lertresLetras(char string[][]) {
-	
-    
-	int i,j;
+	void lertresLetras(char string[]) {
+	char letra1;
+	char letra2;
+	char letra3;
+	int i;
 	printf("digite tres letras:\n ") ;
-	scanf("%s",string[3][50]);
+	fflush(stdin);
+	scanf("%s", &letra1) ;
 
-	
-    return string;
-	
-       
-}
+	string[0]=letra1;
+	printf("digite tres letras:\n ") ;
+	fflush(stdin);
+	scanf("%s", &letra2) ;
+	string[1]=letra2;
+	printf("digite tres letras:\n ") ;
+	fflush(stdin);
+	scanf("%s",&letra3) ;
+	string[2]=letra3;
+	for(i=0;i<3;i++){
+	 printf("letras da string:%c\n",string[i]);
+	 
+	 
+	}
+	}
 
-int main(){
-        
-        int i,j;
-	   char string[3][50];
-       
-	    string=lertresLetras();
-        for(i=0;i<3;i++){
-            for(j=0;j<3;j++){
-                printf("frases da string: %s\n",string[i][j]);
-            
-            
-            }
-        }
-}
+	int main(){
+	char string[4];
+	lertresLetras(string);
+	printf("As letras sao: %s\n", string);
+
+
+
+	}
 
 
 

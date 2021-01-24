fa1a1f44df4d0071cb6a4238327da2563f3c477f
luis-garridodev <proganalistaluis@gmail.com>
2021-01-24 17:52:55 -0300

    projeto escola parte 2 finalizada

--- atividades/escola/V02.c
+++ atividades/escola/V02.c
@@ -41,6 +41,7 @@ typedef struct disciplinas
 /*================ INICIO declaraco de funcao =================*/
 void leitordecadastro(Aluno n);
 void listarmateria();
+
 /*================ FIM declaracao de funcao =================*/
 
 /*================ INICIO DE PROCEDIMENTOS =================*/
@@ -53,30 +54,30 @@ void listarmateria();
 void cadastrarAluno(Aluno listaAlunos[TAM], int *contAluno)
 {
 	printf("Fazer o cadastro do aluno\n");
-			
+	
 	printf("digite o numero de matricula:\n");
 	scanf("%d", &listaAlunos[*contAluno].matricula);
-
+	
 	printf("digite o nome do aluno:\n");
 	scanf("%s", listaAlunos[*contAluno].nome);
 	getchar();
-
+	
 	fflush(stdin);
-
+	
 	printf("digite data de nascimento do aluno:\n");
 	scanf("%s", listaAlunos[*contAluno].datadenascimento);
 	getchar();
-
+	
 	fflush(stdin);
-
+	
 	printf("digite o seu cpf:\n");
 	scanf("%d", &listaAlunos[*contAluno].cpf);
-
+	
 	printf("digite m para homem ou f para mulher:\n");
 	scanf("%s", listaAlunos[*contAluno].sexo);
-
+	
 	getchar();
-
+	
 	fflush(stdin);
 	*contAluno = *contAluno + 1;
 	
@@ -119,7 +120,7 @@ void operacoesAluno(Aluno listaAlunos[TAM], int *contAluno)
 		switch (menu)
 		{	
 			case 0:
-				break;
+			break;
 			case 1:
 			{
 				cadastrarAluno(listaAlunos, contAluno);
@@ -138,7 +139,7 @@ void operacoesAluno(Aluno listaAlunos[TAM], int *contAluno)
 				break;
 			}
 			default:
-				printf("opcao invalida");
+			printf("opcao invalida");
 		}
 	} while (menu != 0);
 }
@@ -147,38 +148,38 @@ void operacoesAluno(Aluno listaAlunos[TAM], int *contAluno)
 void cadastroprofessor(cadasProf listaprofessor[TAM],int *contprofessor)
 {
 	
-printf("Fazer o cadastro do professor\n");
-			
+	printf("Fazer o cadastro do professor\n");
+	
 	printf("digite o numero de matricula:\n");
 	scanf("%d", &listaprofessor[*contprofessor].matricula);
-
+	
 	printf("digite o nome do professor:\n");
 	scanf("%s", listaprofessor[*contprofessor].nome);
 	getchar();
-
+	
 	fflush(stdin);
-
+	
 	printf("digite data de nascimento do professor:\n");
 	scanf("%s", listaprofessor[*contprofessor].datadenascimento);
 	getchar();
-
+	
 	fflush(stdin);
-
+	
 	printf("digite o seu cpf:\n");
 	scanf("%d", &listaprofessor[*contprofessor].cpf);
-
+	
 	printf("digite m para homem ou f para mulher:\n");
 	scanf("%s", listaprofessor[*contprofessor].sexo);
-
+	
 	getchar();
-
+	
 	fflush(stdin);
 	*contprofessor = *contprofessor + 1;
 	
 }
 void listarprofessor(cadasProf listaprofessor[TAM],int *contprofessor){
-
-  printf("Lista dos professores\n");
+	
+	printf("Lista dos professores\n");
 	for(int i=0;i<*contprofessor;i++){
 		printf("## professor %d ##\n", i + 1);
 		printf("numero de matricula:%d\n", listaprofessor[i].matricula);
@@ -186,27 +187,35 @@ void listarprofessor(cadasProf listaprofessor[TAM],int *contprofessor){
 		printf("data de nascimento do professor:%s\n",listaprofessor[i].datadenascimento);
 		printf("cpf do professor:%d\n", listaprofessor[i].cpf);
 		printf("sexo do professor:%s\n", listaprofessor[i].sexo);
-
+		
+	}
 }
+void pesquisarmatricula(cadasProf listaprofessor[TAM] , int *contprofessor){
+	int procura;
+	int sair = 1;
+	
+	
+    do
+    { 
+		printf("Numero da matricula que deseja procurar \n");
+		scanf("%d" ,&procura);
+		
+		for(int i=0; i<*contprofessor; i++){
+			if(listaprofessor[i].matricula == procura)
+			printf(" Matricula Correta: \n %d \n" , listaprofessor[i].matricula);
+			else{
+				printf(" Matricula Inexistente\n");  
+			}  
+		}
+		printf(" Para Continuar: digite qualquer numero\n Sair: digite 0\n ");  
+		scanf("%d" , &sair);  
+	}while(sair!=0);
+	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 void operacoesprofessor(cadasProf listaprofessor[TAM] ,  int *contprofessor){
-
-int menu;
+	
+	int menu;
 	printf("### professor ####\n");
 	
 	do
@@ -216,13 +225,14 @@ int menu;
 		printf("[1] -> Cadastrar\n");
 		printf("[2] -> Listar\n");
 		printf("[3] -> Excluir\n");
+		printf("[4] -> Listar Matricula\n");
 		
 		scanf("%d", &menu);
 		
 		switch (menu)
 		{	
 			case 0:
-				break;
+			break;
 			case 1:
 			{
 				cadastroprofessor(listaprofessor , contprofessor);
@@ -240,54 +250,58 @@ int menu;
 				excluirAluno();
 				break;
 			}
+			case 4:
+			{
+				pesquisarmatricula(listaprofessor,contprofessor);
+				break;
+			}
 			default:
-				printf("opcao invalida");
+			printf("opcao invalida");
 		}
 	} while (menu != 0);
-
-
-
-
-
+	
+	
+	
+	
+	
 }
-
 
 //começo com perações com matéria//
 void cadastromateria(materia listamateria[TAM], int *contmateria)
 {
   	printf("Fazer o cadastro da materia\n");
-			
+	
 	printf("digite onome da materia:\n");
 	scanf("%s", listamateria[*contmateria].nome);
 	getchar();
-
+	
 	fflush(stdin);
 	printf("digite o codigo da materia:\n");
 	scanf("%d", &listamateria[*contmateria].codigo);
-
-
+	
+	
 	printf("digite o semestre:\n");
 	scanf("%d", &listamateria[*contmateria].semestre);
 	
-
-
+	
+	
 	printf("digite o professor que leciona:\n");
 	scanf("%s", listamateria[*contmateria].professor);
-
+	
 	getchar();
-
+	
 	fflush(stdin);
 	*contmateria = *contmateria + 1;
 	
-
-
-
-
-
+	
+	
+	
+	
+	
 }
 void listarmateria(materia listamateria[TAM],int *contmateria){
-
-  printf("Lista da materia\n");
+	
+	printf("Lista da materia\n");
 	for(int i=0;i<*contmateria;i++){
 		printf("## professor %d ##\n", i + 1);
 		printf("nome da materia:%s\n", listamateria[i].nome);
@@ -295,8 +309,8 @@ void listarmateria(materia listamateria[TAM],int *contmateria){
 		printf("semestre:%d\n",listamateria[i].semestre);
 		
 		printf("nome do professor:%s\n", listamateria[i].professor);
-
-}
+		
+	}
 }
 void operacoesmateria(materia listamateria[TAM], int *contmateria)
 {
@@ -316,7 +330,7 @@ void operacoesmateria(materia listamateria[TAM], int *contmateria)
 		switch (menu)
 		{	
 			case 0:
-				break;
+			break;
 			case 1:
 			{
 				cadastromateria(listamateria, contmateria);
@@ -335,14 +349,10 @@ void operacoesmateria(materia listamateria[TAM], int *contmateria)
 				break;
 			}
 			default:
-				printf("opcao invalida");
+			printf("opcao invalida");
 		}
 	} while (menu != 0);
 }
-
-
-
-
 
 
 /*================ INICIO MAIN =================*/
@@ -355,10 +365,10 @@ int main()
 	cadasProf d;
 	Aluno listaAlunos[TAM];
 	int contAluno = 0;
-  cadasProf listaprofessor[TAM];
-  int contprofessor=0;
+	cadasProf listaprofessor[TAM];
+	int contprofessor=0;
 	materia listamateria[TAM];
-  int contmateria=0;
+	int contmateria=0;
 	//menu de interaco excluir, cadastra e listaraluno
 	
 	do
@@ -382,16 +392,17 @@ int main()
 			case 2:
 			{
 				printf("opcoes de professor\n");
-        operacoesprofessor(listaprofessor,&contprofessor);
-        
+				operacoesprofessor(listaprofessor,&contprofessor);
+				
 				break;
 			}
 			case 3:
 			{
 				printf("opcoes de disciplina\n");
-          operacoesmateria(listamateria, &contmateria);
+				operacoesmateria(listamateria, &contmateria);
 				break;
 			}
 		}
 	} while (menu != -1);
 }
+
